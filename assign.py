cf5de3c7e441c8c8e7aa15d25bdaf47241c3d643
Max Hutchinson <maxhutch@gmail.com>
2015-06-22 08:22:50 -0500

    Almost all cleaning, associated with adding all the honors online.

--- assign.py
+++ assign.py
@@ -7,13 +7,20 @@ from web_io import get_sheet
 from conf import members_url, honors_url
 
 honors = get_sheet(honors_url)
-members = get_sheet(members_url, 0) 
+members = get_sheet(members_url) 
 
 """ Clean up! """
+shabbat = False
 members['Tribe'] = members['Tribe'].fillna('Israel')
+honors['Name'] = honors['Name'].fillna("Delete")
+honors = honors[honors.Name != "Delete"]
 honors['Tribe'] = honors['Tribe'].fillna('Israel')
-honors['Hebrew'] = honors['Type'] == 'Aliyah'
-
+honors['Hebrew'] = (honors['Type'] == 'Aliyah')
+honors['Shabbat'] = honors['Shabbat'].fillna('Any')
+if shabbat:
+  honors = honors[honors.Shabbat != 'Exclude']
+else:
+  honors = honors[honors.Shabbat != 'Only']
 
 print(honors)
 print(members)
@@ -21,27 +28,34 @@ print(members)
 rank = max(honors.shape[0], members.shape[0])
 scores = np.zeros((rank, rank))
 
-j = 0
-trans = []
-for name,mem in members.iterrows():
-  trans.append(name)
+this_year = 2015
+year_scale = 2
+
+for j in range(members.shape[0]):
+  i = 0
+  mem = members.iloc[j]
   for i in range(honors.shape[0]):
-    honor = honors.loc[i]
-    if honor['Tribe'] != 'Israel' and mem['Tribe'] != honors.loc[i]['Tribe']:
+    honor = honors.iloc[i]
+    if honor['Tribe'] != 'Israel' and mem['Tribe'] != honor['Tribe']:
       continue
     if honor['Hebrew'] and not mem['Hebrew']:
       continue
-    scores[i, j] = 1.
-  j = j + 1
+    scores[j, i] = 1.
+  scores[j,:] *= (this_year - mem['Last Honor']) * year_scale
+  if this_year - mem['Joined'] <= 1:
+    scores[j,:] *= year_scale
 
-#print(scores)
+print(scores)
 hung = Hungarian(scores, is_profit_matrix=True)
 hung.calculate()
 
 results = hung.get_results()
+# Optimal outcome is the members with the highest maximum score being assigned to the maximal part
+opt_potential = np.sum(np.sort(np.max(scores, axis=1))[-min(honors.shape[0],members.shape[0]):])
+
 for res in results:
-  if res[1] >= len(trans):
-    break
-  print("{:s} is assigned to {:s}".format(trans[res[1]], honors.loc[res[0]]['Name']))
-print("Total score is {:f}".format(hung.get_total_potential()))
+  if res[1] >= honors.shape[0] or res[0] >= members.shape[0]:
+    continue
+  print("{:20s} is assigned to {:s} for {:s}".format(members.iloc[res[0]].Name, honors.iloc[res[1]].Name, honors.iloc[res[1]].Service))
+print("Total score is {:f} of {:f}".format(hung.get_total_potential(), opt_potential))
 
