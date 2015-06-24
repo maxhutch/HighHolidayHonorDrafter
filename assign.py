7479d9c4951e3876e757e73a4609e96504b1c79a
Max Hutchinson <maxhutch@gmail.com>
2015-06-24 16:02:36 -0500

    Added member-honoree units (mhu, read: families) to distribute honors.
    Honors are now assigned to member-honoree units (mhu), which
    are generally families.  No more than one honor is assigned per mhu.
    Therefore, certain members whose honors should not "use" the
    honor designated for their families should be put in their own mhu.
    Any member not being listed in an mhu is automatically put in an
    mhu of their own.

--- assign.py
+++ assign.py
@@ -4,10 +4,12 @@ import pandas as pd
 import numpy as np
 from hungarian_algorithm.hungarian import *
 from web_io import get_sheet
-from conf import members_url, honors_url
+from conf import members_url, honors_url, mhu_url, categories_url
 
 honors = get_sheet(honors_url)
 members = get_sheet(members_url) 
+mhus = get_sheet(mhu_url)
+cats = get_sheet(categories_url)
 
 """ Clean up! """
 shabbat = False
@@ -24,38 +26,76 @@ else:
 
 print(honors)
 print(members)
+print(cats)
+print(cats.columns.values)
+print(mhus)
+name_to_mhu = {}
+for i in range(mhus.shape[0]):
+  mhu = mhus.iloc[i]
+  for name in list(mhu):
+    name_to_mhu[name] = i
+i = mhus.shape[0]
+for name in list(members.Name):
+  if name in name_to_mhu:
+    continue
+  name_to_mhu[name] = i
+  mhus = mhus.append(pd.DataFrame([{"Member": name},]),ignore_index=True)
+  i = i + 1
+print(mhus)
 
-rank = max(honors.shape[0], members.shape[0])
-scores = np.zeros((rank, rank))
+rank = max(honors.shape[0], mhus.shape[0])
+scores_individual = np.zeros((members.shape[0], rank))
+scores_mhu = np.zeros((rank, rank))
 
 this_year = 2015
-year_scale = 2
+board_member = 8
+new_member   = 7
+two_year     = 6
+one_year     = 5
 
+name_to_member = {}
 for j in range(members.shape[0]):
   i = 0
   mem = members.iloc[j]
+  name_to_member[mem.Name] = j
   for i in range(honors.shape[0]):
     honor = honors.iloc[i]
     if honor['Tribe'] != 'Israel' and mem['Tribe'] != honor['Tribe']:
       continue
     if honor['Hebrew'] and not mem['Hebrew']:
       continue
-    scores[j, i] = 1.
-  scores[j,:] *= (this_year - mem['Last Honor']) * year_scale
+    scores_individual[j, i] = 1.
   if this_year - mem['Joined'] <= 1:
-    scores[j,:] *= year_scale
+    scores_individual[j,:] *= new_member
+  elif (this_year - mem['Last Honor'] == 3):
+    scores_individual[j,:] *= two_year
+  elif (this_year - mem['Last Honor'] == 2):
+    scores_individual[j,:] *= one_year
 
-print(scores)
-hung = Hungarian(scores, is_profit_matrix=True)
+for i in range(mhus.shape[0]):
+  mhu = mhus.iloc[i]
+  for name in list(mhu):
+    if pd.isnull(name):
+      break
+    scores_mhu[i,:] = np.maximum(scores_mhu[i,:], scores_individual[name_to_member[name],:])
+
+print(scores_individual)
+print(scores_mhu)
+hung = Hungarian(scores_mhu, is_profit_matrix=True)
 hung.calculate()
 
 results = hung.get_results()
 # Optimal outcome is the members with the highest maximum score being assigned to the maximal part
-opt_potential = np.sum(np.sort(np.max(scores, axis=1))[-min(honors.shape[0],members.shape[0]):])
+opt_potential = np.sum(np.sort(np.max(scores_mhu, axis=1))[-min(honors.shape[0],mhus.shape[0]):])
 
 for res in results:
-  if res[1] >= honors.shape[0] or res[0] >= members.shape[0]:
+  if res[1] >= honors.shape[0] or res[0] >= mhus.shape[0]:
     continue
-  print("{:20s} is assigned to {:s} for {:s}".format(members.iloc[res[0]].Name, honors.iloc[res[1]].Name, honors.iloc[res[1]].Service))
+  winner = "No One"; best = 0
+  for name in list(mhus.iloc[res[0]]):
+    if (not pd.isnull(name)) and scores_individual[name_to_member[name], res[1]] > best:
+      winner = name
+      best = scores_individual[name_to_member[name], res[1]] 
+  print("{:20s} is assigned to {:s} for {:s}".format(winner, honors.iloc[res[1]].Name, honors.iloc[res[1]].Service))
 print("Total score is {:f} of {:f}".format(hung.get_total_potential(), opt_potential))
 
